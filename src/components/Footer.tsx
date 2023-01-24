934f16685a6fd9d01d938f4f4683c822c9061e3b
BuzzKill Master <BuzzKillMaster@proton.me>
2023-01-24 13:16:51 +0100

    add horizontal padding to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {motion} from "framer-motion";
 
 export default function Footer() {
     return (
-        <motion.footer layout className={"py-8 text-center text-sm uppercase font-semibold"}>
+        <motion.footer layout className={"p-8 text-center text-sm uppercase font-semibold"}>
             <p>Humbly inspired by <a className={"text-emerald-500 hover:text-emerald-400 hover:underline"}
                                      href="https://brittanychiang.com/" target={"_blank"} rel={"noreferrer"}>Brittany
                 Chiang</a>, created by Christian Pedersen</p>
