49edf8c20b3a171f7c1a18e103bbb66578c4f3fb
BuzzKill Master <buzzkillmaster@proton.me>
2023-03-20 07:40:34 +0100

    improve copy in landing section to better convey personality

--- src/components/sections/Landing.tsx
+++ src/components/sections/Landing.tsx
@@ -10,7 +10,7 @@ export default function LandingSection() {
 
             <p className={"mt-8 lg:w-1/2 mb-12"}>Which I do with quite the enthusiasm, and also a tiny bit of
                 absurdity. In a world of highly skilled individuals, I stand out based on my extroverted and
-                fun-loving personality -&nbsp;as well as my capabilities.</p>
+                fun-loving personality, not just my capabilities.</p>
 
             <p onClick={() => scrollToView("about")}
                className={"py-4 px-6 border-2 rounded font-semibold cursor-pointer border-emerald-400 text-emerald-400 hover:bg-emerald-400 hover:text-slate-900"}>Get
